7b10a9e9d225f479b37c6dfa9c0fa037462a55cb
Lourdesbernal <162721610+Lourdesbernal@users.noreply.github.com>
2024-04-19 00:59:22 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,25 +1,26 @@
 import math
 def line():
-        
-        a=float(input("Ingrese el coeficiente A:"))
-        b=float(input("Ingrese el coeficiente B:"))
-        X1=float(input("Ingrese el coeficiente X1:"))
-        X2=float(input("Ingrese el coeficiente X2:"))
-        Y1=float(a*X1+b)
-        Y2=float(a*X2+b)
-        P1=[X1 , Y1]
-        P2=[X2 , Y2]
 
-        print(f"El coeficiente A de su ecuación de la recta es: {a}")
-        print(f"El coeficiente B de su ecuación de la recta es: {b}")
-        print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
-        print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
-        print()
-        print("Para la siguiente ecuacion:")
-        print(f"\tY = {a}x + {b}")
-        print()
-        print("Dados los siguientes puntos:")
-        print(f"\tP1 ({X1}, {Y1})")
-        print(f"\tP2 ({X2}, {Y2})")
-        print()
-        print("La distancia entre ellos es: " + str(math.dist(P1, P2)))
+
+    a=float(input("Ingrese el coeficiente A:"))
+    b=float(input("Ingrese el coeficiente B:"))
+    X1=float(input("Ingrese el coeficiente X1:"))
+    X2=float(input("Ingrese el coeficiente X2:"))
+    Y1=float(a*X1+b)
+    Y2=float(a*X2+b)
+    P1= [X1 , Y1]
+    P2=[X2 , Y2]
+
+    print(f"El coeficiente A de su ecuación de la recta es: {a}")
+    print(f"El coeficiente B de su ecuación de la recta es: {b}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
+    print()
+    print("Para la siguiente ecuación:")
+    print(f"\tY = {a}X + {b}")
+    print()
+    print("Dados los siguientes puntos:")
+    print(f"\tP1 ({X1}, {Y1})")
+    print(f"\tP2 ({X2}, {Y2})")
+    print()
+    print("La distancia entre ellos es: " + str(math.dist(P1, P2)))
